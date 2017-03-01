30986b5f65e35bcfb10c2976e7c83810047f0e65
sos161712avj <the.numetal@gmail.com>
2017-03-01 11:33:11 +0000

    Arreglado issue, close #12

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var port = (process.env.PORT || 8095);
 var dateFormat = require('dateformat');
-var time = new Date();
+//var time = new Date(); No necesario. 
 
 app.listen(port, (err) => {
     if (!err)
@@ -12,7 +12,7 @@ app.listen(port, (err) => {
 });
 
 app.get("/time", (req, res) => {
-    res.send("<html><body><h1>" + '"' + dateFormat(time, "dS mmmm 'of' yyyy, HH:MM:ss") + '"' + "</h1></body><html>");
+    res.send("<html><body><h1>" + '"' + dateFormat("dS mmmm 'of' yyyy, HH:MM:ss") + '"' + "</h1></body><html>");
 });
 
 app.get("/", (req, res) => {
