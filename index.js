85099dcf67e2f6d6613084913cb6cfe590dca317
sos161712jhh <jmhh66@ono.com>
2017-03-05 19:26:22 +0000

    Servidor esxtendido con carpeta 'public' rel #14

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var express = require("express");
-var app = express();
-var port = (process.env.PORT || 8095);
 var dateFormat = require('dateformat');
-//var time = new Date(); No necesario. 
+var path = require("path");
 
-app.listen(port, (err) => {
-    if (!err)
-        console.log("Server initialized on port " + port);
-    else
-        console.log("An error ocurred while trying to initialize server: " + err);
-});
+var port = process.env.PORT || 8095;
+
+var app = express();
+
+app.use("/",express.static(path.join(__dirname, "public")));
 
 app.get("/time", (req, res) => {
     res.send("<html><body><h1>" + '"' + dateFormat("dS mmmm 'of' yyyy, HH:MM:ss") + '"' + "</h1></body><html>");
 });
 
-app.get("/", (req, res) => {
-    res.send("<html><body><h1><a href=/time>/time</a></h1></body><html>");
+app.listen(port,() => {
+    console.log("Server initialized on port " + port);
+}).on("error",(e)=>{
+    console.log("Server can not be started: " + e);
+    process.exit(1);
 });
+
+console.log("--------------------------------------");
