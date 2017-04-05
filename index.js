acf1190a40f3ee0549659100c585e9c3275847c3
sos161712hgm <heliogm88@gmail.com>
2017-04-05 01:16:50 +0000

    modularized fs-api 2

--- index.js
+++ index.js
@@ -29,41 +29,8 @@ es.register_AR_api(app);
 ///////////////////API FREE SOFTWARE STATS (www.portalprogramas.com)////////////
 
 var fs = require("./fs-stats.js");
-//Load Initial Data
-app.get(BASE_API_PATH + "/free-software-stats/loadInitialData", fs.loadInitialData);
 
-// GET a collection
-app.get(BASE_API_PATH + "/free-software-stats", fs.getCollection);
-
-// GET a single resource
-app.get(BASE_API_PATH + "/free-software-stats/:university/:year", fs.getSingleResource);
-
-
-// GET a year or university
-app.get(BASE_API_PATH + "/free-software-stats/:resource", fs.getStat);
-
-//POST over a collection
-app.post(BASE_API_PATH + "/free-software-stats", fs.postCollection);
-
-
-//POST over a single resource
-app.post(BASE_API_PATH + "/free-software-stats/:university/:year", fs.postSingleResource);
-
-//PUT over a collection
-app.put(BASE_API_PATH + "/free-software-stats", fs.putCollection);
-
-//PUT over a single resource
-app.put(BASE_API_PATH + "/free-software-stats/:university/:year", fs.putSingleResource);
-
-//DELETE over a collection
-app.delete(BASE_API_PATH + "/free-software-stats", fs.deleteCollection);
-
-
-//DELETE over a single resource
-app.delete(BASE_API_PATH + "/free-software-stats/:university/:year", fs.deleteSingleResource);
-
-//DELETE over a year or university
-app.delete(BASE_API_PATH + "/free-software-stats/:resource", fs.deleteResource);
+fs.register_fs_api(app);
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////API ACADEMIC RANKINGS STATS ////////////////////////////
