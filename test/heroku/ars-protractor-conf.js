a8fea2838ad9b7f2299cc4b3ff17c4bb36b6ff33
sos161712jhh <jmhh66@ono.com>
2017-04-24 09:40:13 +0000

    Deleting test folder and add it to .gitignore

--- test/heroku/ars-protractor-conf.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-    seleniumAddress : 'http://localhost:9515',
-    specs : ['T01-LoadResources.js', 'T02-AddResource.js'],
-    capabilties: {
-        'browserName' : 'phantomjs'
-    }
-};
