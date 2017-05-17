6cba43fa0f397d8acb6e9cd7056aa1496be5e049
sos161712jhh <jmhh66@ono.com>
2017-05-17 11:30:56 +0000

    Integration with proxy

---
+++ public/analytics/ar/integrations.js
@@ -0,0 +1,112 @@
+/* global angular */
+
+angular.
+module("sos1617-12-app")
+    .controller("IntegrationsCtrl", ["$scope", "$http", function($scope, $http) {
+        console.log("HighCharts controller for ARS initilized");
+        $scope.apikey = "018d375e";
+
+        var mediasPorAño = [];
+        var pisaPorAño = [];
+        var series = [];
+
+        $http.get("/api/v1/pisa/").then(function(response) {
+            var pisa;
+            for (var a = 2007; a < 2017; a++) {
+                pisa = response.data.filter(function(current) {
+                    return current.country == 'Spain' && Number(current.year) == a;
+                });
+                if (pisa.length == 0) {
+                    pisa = [{
+                        "country": "Spain",
+                        "math": "0",
+                        "reading": "0",
+                        "science": "0",
+                    }];
+                }
+                pisa = pisa.map(function(current) {
+                    return Number(current.math) + Number(current.reading) + Number(current.science);
+                });
+                pisaPorAño.push(pisa[0]);
+            }
+
+            $http.get("/api/v1/academic-rankings-stats?apikey=" + $scope.apikey).then(function(response) {
+                for (var a = 2007; a < 2017; a++) {
+                    var media;
+                    media = response.data.filter(function(current) {
+                        return current.year == a;
+                    });
+                    media = media.map(function(current) {
+                        return current.world_position;
+                    });
+                    media = media.reduce(function(b, c) {
+                        return b + c;
+                    }, 0) / response.data.length;
+
+                    mediasPorAño.push(media);
+                }
+
+
+                series.push({
+                    'name': 'Year average rankings',
+                    'data': mediasPorAño,
+                });
+                series.push({
+                    'name': 'Pisa results for Spain',
+                    'data': pisaPorAño,
+                });
+                Highcharts.chart('container', {
+                    chart: {
+                        type: 'area'
+                    },
+                    title: {
+                        text: 'Pisa results and average of academic rankings'
+                    },
+                    subtitle: {
+                        text: 'Source: various'
+                    },
+                    xAxis: {
+                        categories: ['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'],
+                        tickmarkPlacement: 'on',
+                        title: {
+                            enabled: false
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: ''
+                        },
+                        labels: {
+                            formatter: function() {
+                                return this.value;
+                            }
+                        }
+                    },
+                    tooltip: {
+                        split: true,
+                        valueSuffix: ' millions'
+                    },
+                    plotOptions: {
+                        area: {
+                            stacking: 'normal',
+                            lineColor: '#666666',
+                            lineWidth: 1,
+                            marker: {
+                                lineWidth: 1,
+                                lineColor: '#666666'
+                            }
+                        }
+                    },
+
+                    series: series
+
+                });
+
+            });
+
+        });
+
+
+
+
+    }]);
