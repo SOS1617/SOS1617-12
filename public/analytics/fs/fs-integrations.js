d97efa6dcd114c5b76e57fce8b0b34aa13348b5c
sos161712hgm <heliogm88@gmail.com>
2017-05-26 16:47:01 +0000

    first fs use

--- public/analytics/fs/fs-integrations.js
+++ public/analytics/fs/fs-integrations.js
@@ -7,11 +7,16 @@ module("sos1617-12-app")
 
         $scope.poem = "";
 
-        $http.get("https://pafmon-walt-whitman-poems.p.mashape.com/poems/o-me-o-life")
-            .header("X-Mashape-Key", "Ct017yQKOzmshgTzaOjqqmKHCr5Pp1yMhB9jsnbmSsC5ReNvjh")
-            .header("Accept", "application/json").end(function(result) {
-                console.log(result.status, result.headers, result.body);
-            }).then(function(response) {
+        $http.get('https://pafmon-walt-whitman-poems.p.mashape.com/poems/o-me-o-life', {
+                headers: {
+                    "X-Mashape-Key": "Ct017yQKOzmshgTzaOjqqmKHCr5Pp1yMhB9jsnbmSsC5ReNvjh"
+                }
+            })
+            .then(function(response) {
+
+
+
+
                 $scope.poem = response.data;
 
             });
