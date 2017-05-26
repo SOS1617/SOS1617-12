a13165214294e6f1d13469ccae5eaa6b907c160c
sos161712hgm <heliogm88@gmail.com>
2017-05-26 16:15:37 +0000

    fix fs integr contr

--- public/analytics/fs/fs-integrations.js
+++ public/analytics/fs/fs-integrations.js
@@ -1,20 +1,32 @@
-angular.module("sos1617-12").controller("FSIntegrations", ["$scope", "$http", function($scope, $http) {
-    console.log("Integrations controller for FS initilized");
-    
-    $scope.poems = "";
+/* global angular */
 
-    $http.get("https://pafmon-walt-whitman-poems.p.mashape.com/poems/o-me-o-life")
-        .header("X-Mashape-Key", "Ct017yQKOzmshgTzaOjqqmKHCr5Pp1yMhB9jsnbmSsC5ReNvjh")
-        .header("Accept", "application/json").then(function(response) {
-            $scope.poems = response.data;
+angular.
+module("sos1617-12-app")
+    .controller("FSIntegrationCtrl", ["$scope", "$http", function($scope, $http) {
+        console.log("Integrations controller for FS initilized");
 
-        });
+        $scope.poem = "";
 
-    $http.get("").then(function(response) {
+        $http.get("https://pafmon-walt-whitman-poems.p.mashape.com/poems/o-me-o-life")
+            .header("X-Mashape-Key", "Ct017yQKOzmshgTzaOjqqmKHCr5Pp1yMhB9jsnbmSsC5ReNvjh")
+            .header("Accept", "application/json").end(function(result) {
+                console.log(result.status, result.headers, result.body);
+            }).then(function(response) {
+                $scope.poem = response.data;
 
-    });
+            });
 
-    $http.get("").then(function(response) {
 
-    });
-}]);
+
+
+    }]);
+
+
+
+// $http.get("").then(function(response) {
+
+// });
+
+// $http.get("").then(function(response) {
+
+// });
