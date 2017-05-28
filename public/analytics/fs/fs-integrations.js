f77aae74b06c243d75f8a20319a1ff94ffc356f7
sos161712hgm <heliogm88@gmail.com>
2017-05-28 02:26:14 +0000

    reserva apis

--- public/analytics/fs/fs-integrations.js
+++ public/analytics/fs/fs-integrations.js
@@ -5,9 +5,9 @@ module("sos1617-12-app")
     .controller("FSIntegrationCtrl", ["$scope", "$http", function($scope, $http) {
         console.log("Integrations controller for FS initilized");
 
+        //01. Walt Whitman Poems
         $scope.poem = "";
         var listPoem = [];
-
 
         $http.get('https://pafmon-walt-whitman-poems.p.mashape.com/poems/', {
                 headers: {
@@ -35,15 +35,27 @@ module("sos1617-12-app")
                 });
         };
 
+        //A usar (Entertaiment Mashape Apis)
+        //02. Random Famous Quotes
+        
+        //03. Love Calculator
+       
+        //04. Robohash Image Generator
+       
+        //05. Poetry DB
+       
+        //06. WikiHow
+       
+        //07. Fancy text
+       
+        //08. Chuck Norris
+
+        //09. Tronald Dump
+      
+        //10. Kitten Placeholder
+
+
+
+
 
     }]);
-
-
-
-// $http.get("").then(function(response) {
-
-// });
-
-// $http.get("").then(function(response) {
-
-// });
